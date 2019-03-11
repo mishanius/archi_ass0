9a6ec3b194ef3e4d67bf8ef4036d5aec4e9fe2c3
mishanius <michael3120@gmail.com>
2019-03-11 23:14:09 +0300

    finished the task

--- asmAssignment0.s
+++ asmAssignment0.s
@@ -20,18 +20,22 @@ do_Str:                        		; do_Str function definition - functions are de
 		jz whiteSpace
 		cmp byte [ecx], 32
 		jz whiteSpace
+		cmp byte [ecx], 97
+		jnz NOT_A
 		xor byte [ecx], 0x20
 	incriment:
 		inc ecx      	    	; increment ecx value; now ecx points to the next character of the string
-		cmp byte [ecx+1], 0   	; check if the next character (character = byte) is zero (i.e. null string termination)
+		cmp byte [ecx], 0   	; check if the next character (character = byte) is zero (i.e. null string termination)
 		jnz yourCode
 		jmp BREAK      	; if not, keep looping until meet null termination character
+	NOT_A:
+		cmp byte [ecx], 66
+		jnz incriment
+		xor byte [ecx], 0x20
+		jmp incriment
 	whiteSpace:
-		inc ecx 
 		inc byte [an]
-		cmp byte [ecx+1], 0   	; check if the next character (character = byte) is zero (i.e. null string termination)
-		jnz yourCode
-		jmp BREAK
+		jmp incriment
 	BREAK:
         popad                    	; restore all previously used registers
         mov eax,[an]         		; return an (returned values are in eax)
